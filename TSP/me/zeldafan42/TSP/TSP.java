455ea8aefd6ee71703d2095e8740209af6b7730e
zeldafan42 <zeldafan42@gmail.com>
2015-06-16 17:41:03 +0200

    Sqrt now at start

--- TSP/me/zeldafan42/TSP/TSP.java
+++ TSP/me/zeldafan42/TSP/TSP.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class TSP
 {
@@ -18,19 +15,19 @@ public class TSP
 		TSP tsp = new TSP();
 		tsp.run(args);
 	}
-	
+
 	void run(String[] args)
 	{
-		if (args.length !=2)
+		if (args.length != 2)
 		{
 			System.out.println("USAGE: tsp -e|-n <Path to textfile>");
 			return;
 		}
-			java.util.Locale.setDefault(java.util.Locale.ENGLISH);
-			double adjMtx[][];
-			double coords[][];
-			int count = 0;
-		//Read file
+		java.util.Locale.setDefault(java.util.Locale.ENGLISH);
+		double adjMtx[][];
+		double coords[][];
+		int count = 0;
+		// Read file
 		try
 		{
 			Scanner sc = new Scanner(Paths.get(args[1], new String[0]));
@@ -46,7 +43,7 @@ public class TSP
 				return;
 			}
 
-			if(count < 1)
+			if (count < 1)
 			{
 				sc.close();
 				System.err.println("Count too small!!!!111");
@@ -55,13 +52,13 @@ public class TSP
 
 			coords = new double[count][2];
 
-			for(int i = 0; i< count; i++)
+			for (int i = 0; i < count; i++)
 			{
-				for(int j = 0;j<2;j++)
+				for (int j = 0; j < 2; j++)
 				{
-					if(sc.hasNextDouble())
+					if (sc.hasNextDouble())
 					{
-						coords[i][j] = sc.nextDouble(); 
+						coords[i][j] = sc.nextDouble();
 					}
 					else
 					{
@@ -71,17 +68,17 @@ public class TSP
 					}
 				}
 			}
-			
-//			System.out.println(count);
-//			
-//			for(int i = 0; i< count; i++)
-//			{
-//				for(int j = 0;j<2;j++)
-//				{
-//					System.out.print(coords[i][j]+" ");
-//				}
-//				System.out.print(System.lineSeparator());
-//			}
+
+			// System.out.println(count);
+			//
+			// for(int i = 0; i< count; i++)
+			// {
+			// for(int j = 0;j<2;j++)
+			// {
+			// System.out.print(coords[i][j]+" ");
+			// }
+			// System.out.print(System.lineSeparator());
+			// }
 			sc.close();
 		}
 		catch (IOException e)
@@ -89,155 +86,171 @@ public class TSP
 			e.printStackTrace();
 			return;
 		}
-		
+
 		adjMtx = new double[count][count];
 
-
-		int j = 0;
 		double distance = 0;
-		
-		for(j=0; j<count;j++)
+
+		for (int j = 0; j < count; j++)
 		{
-			for(int i=j;i<count;i++)
-			{
-				if(i==j)
+			for (int i = j; i < count; i++)
+		{
+				if (i == j)
 				{
 					adjMtx[i][i] = 0;
 				}
 				else
 				{
-					distance = calcDistance(coords[i],coords[j]);
+					distance = calcDistance(coords[i], coords[j]);
 					adjMtx[j][i] = distance;
 					adjMtx[i][j] = distance;
 				}
 			}
 		}
-		
-//		for(double[] arr : adjMtx)
-//		{
-//			NumberFormat nf = new DecimalFormat("00.00");
-//			for(double d : arr)
-//			{
-//				System.out.print(nf.format(d) + " ");
-//			}
-//			System.out.print(System.lineSeparator());
-//		}
-		
+
+		// for(double[] arr : adjMtx)
+		// {
+		// NumberFormat nf = new DecimalFormat("00.00");
+		// for(double d : arr)
+		// {
+		// System.out.print(nf.format(d) + " ");
+		// }
+		// System.out.print(System.lineSeparator());
+		// }
+
 		ThreadMXBean thx = ManagementFactory.getThreadMXBean();
 		long start = thx.getCurrentThreadCpuTime();
-		if(args[0].equals("-e"))
+		if (args[0].equals("-e"))
 		{
 			int[] sequence = new int[count];
 			int temp[] = new int[count];
-			enumerate(adjMtx, count, temp, sequence, new boolean[count],0,0,0);
-			for(int i : sequence)
+			enumerate(adjMtx, count, temp, sequence, new boolean[count], 0, 0,0);
+			minLength = 0;
+			for (int i : sequence)
 			{
 				System.out.println("<" + coords[i][0] + "," + coords[i][1] + ">");
 			}
-			System.out.println("Length: " + calcLength(sequence,adjMtx));
+			double length = calcLength(sequence, adjMtx);
+			System.out.println("<" + coords[0][0] + "," + coords[0][1] + ">");
+			System.out.println("Length: " + length);
 			System.out.println("-------");
-		}
-		else if(args[0].equals("-n"))
+		} 
+		else if (args[0].equals("-n"))
 		{
 			int[][] solution = new int[count][count];
-			double bestRoute;
-			
-			for(int i=0; i<count; i++)
+			double bestRoute = -1;
+			int bestSolution = 0;
+
+			for (int i = 0; i < count; i++)
 			{
 				solution[i] = neighbourSearch(adjMtx, count, i);
 			}
-			
-			for(int[] arr : solution)
+
+			for (int i = 0; i < solution.length; i++)
 			{
-				for(int i : arr)
+				for (int j : solution[i])
 				{
-					System.out.println("<" + coords[i][0] + "," + coords[i][1] + ">");
+					System.out.println("<" + coords[j][0] + "," + coords[j][1] + ">");
 				}
-				System.out.println("Length: " + calcLength(arr,adjMtx));
+				double length = calcLength(solution[i], adjMtx);
+				if (length < bestRoute || bestRoute == -1)
+				{
+					bestRoute = length;
+					bestSolution = i;
+				}
+
+				System.out.println("<" + coords[solution[i][0]][0] + ","
+						+ coords[solution[i][0]][1] + ">");
+				System.out.println("Length: " + length);
 				System.out.println("-------");
 			}
-			
-			
+
+			System.out.println("Best Route found: " + System.lineSeparator());
+
+			for (int i : solution[bestSolution])
+			{
+				System.out.println("<" + coords[i][0] + "," + coords[i][1] + ">");
+			}
+			System.out.println("<" + coords[solution[bestSolution][0]][0] + "," + coords[solution[bestSolution][0]][1] + ">");
+			System.out.println("Length: " + bestRoute);
+			System.out.println("-------");
+
 		}
 		else
 		{
 			System.err.println("Algorithm not supported");
 			return;
 		}
-		double end = (thx.getCurrentThreadCpuTime() -start)/1E9;
+		double end = (thx.getCurrentThreadCpuTime() - start) / 1E9;
 		System.out.println("Algorithm execution time: " + end + " seconds");
-		
+
 	}
 
-
-
-	double calcDistance(double begin[],double end[])
+	double calcDistance(double begin[], double end[])
 	{
 		double x = end[0] - begin[0];
 		double y = end[1] - begin[1];
-		return (x*x+y*y);
+		return Math.sqrt(x * x + y * y);
 	}
-	
-	void enumerate(double adjMtx[][],int count,int curtour[], int mintour[], boolean visited[], double sum,int start, int step)
+
+	void enumerate(double adjMtx[][], int count, int curtour[], int mintour[], boolean visited[], double sum, int start, int step)
 	{
 		curtour[step] = start;
 		
-		for(int i = 1;i<count;i++)
-		{
-			if(!visited[i])
-			{
-				visited[i] = true;
-				sum += adjMtx[start][i];
-				step++;
-				//System.out.println(i);
-				enumerate(adjMtx,count,curtour,mintour,visited,sum,i,step);
-				visited[i] = false;
-				step--;
-				sum -= adjMtx[start][i];
-			}
-		}
-		if(step == count-1)
+		if (step == count - 1)
 		{
 			sum += adjMtx[start][0];
-			//System.out.println(sum);
+//			System.out.println(sum);
 //			for(int i : curtour)
 //			{
 //				System.out.print(i);
 //			}
 //			System.out.println();
-			if(minLength == 0 || sum < minLength)
+			if (minLength == 0 || sum < minLength)
 			{
 				minLength = sum;
-				System.arraycopy(curtour,0,mintour,0,count);
-//				for(int i : curtour)
-//				{
-//					System.out.print(i);
-//				}
-//				System.out.println();
+				System.arraycopy(curtour, 0, mintour, 0, count);
+				// for(int i : curtour)
+				// {
+				// System.out.print(i);
+				// }
+				// System.out.println();
 			}
 		}
-		
+
+		for (int i = 1; i < count; i++)
+		{
+			if (!visited[i])
+			{
+				visited[i] = true;
+				sum += adjMtx[start][i];
+				step++;
+				// System.out.println(i);
+				enumerate(adjMtx, count, curtour, mintour, visited, sum, i,
+						step);
+				visited[i] = false;
+				step--;
+				sum -= adjMtx[start][i];
+			}
+		}
 	}
-	
+
 	double calcLength(int[] route, double[][] adjMtx)
 	{
 		double sum = 0;
 		int i = 0;
-		
-		for(i=0; i<route.length-1; i++)
+
+		for (i = 0; i < route.length - 1; i++)
 		{
-			sum += Math.sqrt(adjMtx[route[i]][route[i+1]]);
+			sum += adjMtx[route[i]][route[i + 1]];
 		}
 
-		sum += Math.sqrt(adjMtx[route[route.length-1]][route[0]]);
-		
-		
-		
+		sum += adjMtx[route[route.length - 1]][route[0]];
+
 		return sum;
 	}
-	
-	int[] neighbourSearch(double adjMtx[][], int count, int startIndex)
-	{
+
+	int[] neighbourSearch(double adjMtx[][], int count, int startIndex) {
 		int[] res = new int[count];
 		int curElement = startIndex;
 		int tmpElement = 0;
@@ -245,32 +258,26 @@ public class TSP
 		double minCost = 0;
 		int i = 0;
 		boolean visited[] = new boolean[count];
-		
-		for(i=0; i<count; i++)
-		{
+
+		for (i = 0; i < count; i++) {
 			res[i] = -1337;
 			visited[i] = false;
 		}
-		
-		for(i = 0; i<count;i++)
-		{
+
+		for (i = 0; i < count; i++) {
 			res[i] = curElement;
 			visited[curElement] = true;
 			minCost = -1;
-			
-			for(nextElement = 0; nextElement<count; nextElement++)
-			{
-				if(!visited[nextElement])
-				{
-					if(minCost<0)
-					{
+
+			for (nextElement = 0; nextElement < count; nextElement++) {
+				if (!visited[nextElement]) {
+					if (minCost < 0) {
 						minCost = adjMtx[curElement][nextElement];
 						tmpElement = nextElement;
 						continue;
 					}
-					
-					if(minCost > adjMtx[curElement][nextElement])
-					{
+
+					if (minCost > adjMtx[curElement][nextElement]) {
 						minCost = adjMtx[curElement][nextElement];
 						tmpElement = nextElement;
 					}
